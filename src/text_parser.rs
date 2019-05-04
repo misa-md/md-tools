e46d6e9086e0fdc63371c61a479dc6dc68b417fd
genshen <genshenchu@gmail.com>
2019-05-04 23:08:48 +0800

    feat(text): add text format output

--- src/text_parser.rs
+++ src/text_parser.rs
@@ -21,11 +21,18 @@ writer.write_all(b"test\n");
 */
 impl ParseProgress for TextParser {
     fn on_atom_read(&mut self, atom: &OneAtomType) -> i32 {
-        // todo
+        let fmt_string = format!("{} \t{} \t{} \t{} \t{:.6} \t{:.6} \t{:.6}  \t{:.6} \t{:.6} \t{:.6}\n",
+                                 atom.AtomId, atom.Step, atom.getNameByEleName(), atom.InterType,
+                                 atom.Location[0], atom.Location[1], atom.Location[2],
+                                 atom.Velocity[0], atom.Velocity[1], atom.Velocity[2]);
+        self.output.write(fmt_string.as_bytes());
         return 1 as i32;
     }
 
-    fn before_parsing(&self, output: &str) {}
+    fn before_parsing(&mut self, output: &str) {
+        // write header.
+        self.output.write(b"id \tstep \ttype \tinter_type \tlocate.x \tlocate.y \tlocate.z \tv.x \tv.y \tv.z\n");
+    }
 
     fn load_callback(&mut self) -> extern fn(*mut libc::c_void, OneAtomType) -> libc::c_int {
         return callback;
