323c0d74dae3ec8283c9aef284c4340cf1798dd1
genshen <genshenchu@gmail.com>
2019-11-10 20:28:45 +0800

    refactor(voronoy): refactor voronoy.rs
    1. use type alias for float and integer type; 2. add more todo notes.

--- src/ans/voronoy.rs
+++ src/ans/voronoy.rs
@@ -3,14 +3,17 @@ use std::fs::File;
 use xyzio::Reader;
 use std::collections::BTreeMap;
 
-const LATTICE_CONST: f32 = 2.85532;
+type Float = f32;
+type Inx = i32;
+
+const LATTICE_CONST: Float = 2.85532;
 
 /**
  * The coordinate offset of 8 minor lattices surrounding a major lattice.
  * If the major lattice is determined, we can get the lattice coordinate of a minor lattice
  * by this offset.
  */
-const offset: [(i32, i32, i32); 8] = [ //offset index: 0-> x, 1 -> y, 2 -> z
+const offset: [(Inx, Inx, Inx); 8] = [ //offset index: 0-> x, 1 -> y, 2 -> z
     (-1, -1, -1), // x low,  y low,  z low, (b000)
     (1, -1, -1), // x high, y low,  z low, (b001)
     (-1, 0, -1), // x low,  y high, z low, (b010)
@@ -38,7 +41,7 @@ const offset: [(i32, i32, i32); 8] = [ //offset index: 0-> x, 1 -> y, 2 -> z
  *
  * Noticing that, for major lattice position:(0, 0, 0), we always have A*0+B*0+C*0+d < 0.
  */
-const normal_vector: [(f32, f32, f32); 8] = [
+const normal_vector: [(Float, Float, Float); 8] = [
     (-1.0, -1.0, -1.0), // normal vector of vertical bisector plane of (-0.5, -0.5, -0.5) and (0,0,0)
     (1.0, -1.0, -1.0), // (0.5,  -0.5, -0.5) and (0,0,0)
     (-1.0, 1.0, -1.0), // (-0.5, 0.5, -0.5)  and (0,0,0)
@@ -52,7 +55,7 @@ const normal_vector: [(f32, f32, f32); 8] = [
   * d = -(A*x_m + B*y_m + C*z_m), in which (A,B,C) is normal vector (such as (-1, -1, -1) ),
   * (x_m, y_m, z_m) is middle pointer (such as (-1/4, -1/4, -1/4)).
   */
-const d: f32 = -3.0 / 4.0;
+const d: Float = -3.0 / 4.0;
 
 // voronoy analysis method for BCC lattice and cube lattice.
 pub fn voronoy_ans(xyzfile: &str, output: &str) {
@@ -63,38 +66,42 @@ pub fn voronoy_ans(xyzfile: &str, output: &str) {
 
     match snapshot_result {
         Err(e) => {
-            print!("read input xyz file error: {:?}", e);
+            println!("read input xyz file error: {:?}", e);
         }
         Ok(mut snapshot) => {
             let atoms_size = snapshot.size();
             if atoms_size % 2 != 0 { // due to BCC lattice
-                print!("bad atoms size");
+                println!("bad atoms size");
                 return;
             }
             // do analysis
             let size_dim = cube_root(atoms_size / 2);
-            if size_dim == 0 { // it is a cube box
-                do_analysis(size_dim, size_dim, size_dim, &mut snapshot);
+            if size_dim != 0 { // it is a cube box
+                do_analysis(size_dim, size_dim, size_dim, &mut snapshot.atoms);
             } else {
                 let (size_x, size_y, size_z) = get_box_size(&snapshot.atoms);
-                if size_x <= 0 || size_y <= 0 || size_z <= 0 {
-                    print!("bad box size: {},{},{}", size_x, size_y, size_z);
+                if size_x == 0 || size_y == 0 || size_z == 0 {
+                    println!("bad box size");
                     return;
                 }
-                do_analysis(size_x as usize, size_y as usize, size_z as usize, &mut snapshot);
+                if size_x * size_y * size_z != atoms_size {
+                    println!("Warning: box size ({},{},{}) not match atoms size.", size_x, size_y, size_z);
+                }
+                do_analysis(size_x, size_y, size_z, &mut snapshot.atoms);
             }
         }
     }
 }
 
 // return the box size of simulation box to calculate 1D lattice index.
-fn get_box_size(atoms: &Vec<xyzio::Atom>) -> (i32, i32, i32) {
-    let mut x_min: f32 = f32::INFINITY;
-    let mut y_min: f32 = f32::INFINITY;
-    let mut z_min: f32 = f32::INFINITY;
-    let mut x_max: f32 = f32::NEG_INFINITY;
-    let mut y_max: f32 = f32::NEG_INFINITY;
-    let mut z_max: f32 = f32::NEG_INFINITY;
+// if the box size in some dimension is not as desired, 0 will be return in the dimension.
+fn get_box_size(atoms: &Vec<xyzio::Atom>) -> (usize, usize, usize) {
+    let mut x_min = f32::INFINITY; // todo can use f64 as float
+    let mut y_min = f32::INFINITY;
+    let mut z_min = f32::INFINITY;
+    let mut x_max = f32::NEG_INFINITY;
+    let mut y_max = f32::NEG_INFINITY;
+    let mut z_max = f32::NEG_INFINITY;
     for i in 0..atoms.len() {
         let x = atoms[i].x;
         let y = atoms[i].y;
@@ -121,40 +128,66 @@ fn get_box_size(atoms: &Vec<xyzio::Atom>) -> (i32, i32, i32) {
             z_max = z;
         }
     }
+
     let min_index = voronoy(x_min, y_min, z_min);
     let max_index = voronoy(x_max, y_max, z_max);
-    return (max_index.0 - min_index.0, max_index.1 - min_index.1, max_index.2 - min_index.2);
+    let (size_x_, size_y_, size_z_) = (max_index.0 - min_index.0, max_index.1 - min_index.1, max_index.2 - min_index.2);
+    let mut sizes = (0, 0, 0);
+
+    if size_x_ < 0 {
+        sizes.0 = 0;
+    } else {
+        sizes.0 = size_x_ as usize;
+    }
+    if size_y_ < 0 {
+        sizes.1 = 0;
+    } else {
+        sizes.1 = size_y_ as usize;
+    }
+
+    if size_z_ < 0 {
+        sizes.2 = 0;
+    } else {
+        sizes.2 = size_z_ as usize;
+    }
+    return sizes;
 }
 
-fn do_analysis(x_size: usize, y_size: usize, z_size: usize, snapshot: &mut xyzio::Snapshot) {
+// in do_analysis, calculate atom's occupation by box size and its lattice index,
+// then atoms with >= 2 occupation will be logged.
+fn do_analysis(box_x_size: usize, box_y_size: usize, box_z_size: usize, atoms: &Vec<xyzio::Atom>) {
     // scale to lattice const unit.
     let mut atoms_lat_map = BTreeMap::new();
 
-    for i in 0..x_size * y_size * z_size {
+    let atoms_size = atoms.len();
+    for i in 0..atoms_size {
         // calculate lattice index of each atom
         // note: x is doubled.
-        let (x, y, z) = voronoy(snapshot.atoms[i].x, snapshot.atoms[i].y, snapshot.atoms[i].z);
+        let (x, y, z) = voronoy(atoms[i].x, atoms[i].y, atoms[i].z);
+        // todo mode box_x/y/z_size if index is large then box size (todo for user specified box size, not auto size).
+        // todo or mode box_x/y/z_size if real box coord not starting from 0.
+        // which also means: make x,y,z belongs [0, box_x/y/z_size).
         // z * 2 * x_size * y_size  + y * 2 * x_size  + x;
-        let global_index = 2 * (x_size as i32) * (z * y_size as i32 + y) + x;
+        let global_index = 2 * (box_x_size as Inx) * (z * box_y_size as Inx + y) + x;
 
         match atoms_lat_map.get(&global_index) {
             Some(&atom_index) => {
                 if atom_index == -1 {
                     // output i self only
-                    println!("atom: {},{},{},{},{}", snapshot.atoms[i].element, global_index,
-                             snapshot.atoms[i].x, snapshot.atoms[i].y, snapshot.atoms[i].z);
+                    println!("atom: {},{},{},{},{}", atoms[i].element, global_index,
+                             atoms[i].x, atoms[i].y, atoms[i].z);
                 } else {
                     // output i self and data indexed in map
-                    println!("atom: {},{},{},{},{}", snapshot.atoms[atom_index as usize].element, global_index,
-                             snapshot.atoms[atom_index as usize].x, snapshot.atoms[atom_index as usize].y,
-                             snapshot.atoms[atom_index as usize].z);
-                    println!("atom: {},{},{},{},{}", snapshot.atoms[i].element, global_index,
-                             snapshot.atoms[i].x, snapshot.atoms[i].y, snapshot.atoms[i].z);
+                    println!("atom: {},{},{},{},{}", atoms[atom_index as usize].element, global_index,
+                             atoms[atom_index as usize].x, atoms[atom_index as usize].y,
+                             atoms[atom_index as usize].z);
+                    println!("atom: {},{},{},{},{}", atoms[i].element, global_index,
+                             atoms[i].x, atoms[i].y, atoms[i].z);
                     atoms_lat_map.insert(global_index, -1); // first already write
                 }
             }
             None => {
-                atoms_lat_map.entry(global_index).or_insert(i as i32);
+                atoms_lat_map.entry(global_index).or_insert(i as Inx);
             }
         }
     }
@@ -164,14 +197,14 @@ fn do_analysis(x_size: usize, y_size: usize, z_size: usize, snapshot: &mut xyzio
  * calculate coordinate of nearest lattice for a atom:
  * X,Y,Z is the position of atom.
  */
-fn voronoy(x: f32, y: f32, z: f32) -> (i32, i32, i32) {
-    let lat_coord_x = (x / LATTICE_CONST).round() as i32;
-    let mut lat_coord_y = (y / LATTICE_CONST).round() as i32;
-    let mut lat_coord_z = (z / LATTICE_CONST).round() as i32;
+fn voronoy(x: Float, y: Float, z: Float) -> (Inx, Inx, Inx) {
+    let lat_coord_x = (x / LATTICE_CONST).round() as Inx;
+    let mut lat_coord_y = (y / LATTICE_CONST).round() as Inx;
+    let mut lat_coord_z = (z / LATTICE_CONST).round() as Inx;
 
-    let delta_x = x / LATTICE_CONST - (lat_coord_x as f32);
-    let delta_y = y / LATTICE_CONST - (lat_coord_y as f32);
-    let delta_z = z / LATTICE_CONST - (lat_coord_z as f32);
+    let delta_x = x / LATTICE_CONST - (lat_coord_x as Float);
+    let delta_y = y / LATTICE_CONST - (lat_coord_y as Float);
+    let delta_z = z / LATTICE_CONST - (lat_coord_z as Float);
 
     let flag_index = (if delta_z > 0.0 { 4 } else { 0 })
         | (if delta_y > 0.0 { 2 } else { 0 })
