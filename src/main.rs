823be902c4fda0a55f9b4886a7680df8e1a7f4f5
genshen <genshenchu@gmail.com>
2020-12-25 15:54:37 +0800

    feat(analysis): add cli option "verbose" for analysis sub-command(ans) to enable/disable verbose log

--- src/main.rs
+++ src/main.rs
@@ -94,6 +94,11 @@ fn parse_ans(matches: &&clap::ArgMatches) {
         box_size,
     };
 
+    let mut verbose_log: bool = false;
+    if matches.is_present("verbose") {
+        verbose_log = true;
+    }
+
     let mut input_from_minio: bool = false;
     if matches.is_present("input-from-minio") {
         println!("Now we will read input file from minio or AWS s3");
@@ -104,14 +109,16 @@ fn parse_ans(matches: &&clap::ArgMatches) {
         println!("no matching input files");
         return;
     } else if input_files.len() == 1 {
-        ans::analysis::voronoy_ans_wrapper(input_files[0], output, input_from_minio, &box_config)
+        ans::analysis::voronoy_ans_wrapper(input_files[0], output, input_from_minio,
+                                           &box_config, verbose_log)
     } else {
         for input_file in input_files {
             let input_path = Path::new(input_file);
             println!("analysing file {}", input_file);
             let output_suffix = input_path.file_name().unwrap().to_str().unwrap();
             let output_file_path = format!("{}.{}", output, output_suffix);
-            ans::analysis::voronoy_ans_wrapper(input_file, output_file_path.as_str(), input_from_minio, &box_config);
+            ans::analysis::voronoy_ans_wrapper(input_file, output_file_path.as_str(),
+                                               input_from_minio, &box_config, verbose_log);
             println!("file {} analysis, saved at {}", input_file, output_file_path.as_str());
         }
     }
