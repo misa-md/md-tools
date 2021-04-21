a7a70648a2fc781ebc648137bd3769eb8ad48137
genshen <genshenchu@gmail.com>
2021-04-21 17:06:41 +0800

    refactor(diff): use tuple for particles' position and velocity, refactor particles comparison imp
    replace `x,y,z` in struct Particle with tuple `pos: (Real, Real, Real)`, and replace `vx,vy,vz` in
    struct Particle with tuple `v: (Real, Real, Real)`.
    And we refactor the implementation of particles comparison.

--- src/xyz/particle.rs
+++ src/xyz/particle.rs
@@ -9,52 +9,70 @@ use xyzio::Error;
 
 type Real = f64;
 
+// full information of a particle
 pub struct Particle {
     pub id: u32,
-    pub x: Real,
-    pub y: Real,
-    pub z: Real,
-    pub vx: Real,
-    pub vy: Real,
-    pub vz: Real,
+    pub pos: (Real, Real, Real),
+    pub v: (Real, Real, Real),
 }
 
 impl Particle {
-    pub(crate) fn is_near_eq(&self, another: &Particle, error_limit: f64) -> bool {
-        (self.x - another.x).abs() < error_limit &&
-            (self.y - another.y).abs() < error_limit &&
-            (self.z - another.z).abs() < error_limit &&
-            (self.vx - another.vx).abs() < error_limit &&
-            (self.vy - another.vy).abs() < error_limit &&
-            (self.vz - another.vz).abs() < error_limit
+    // check 2 atoms' position with an error limit
+    pub(crate) fn is_pos_near_eq(&self, another: &Particle, error_limit: f64) -> bool {
+        return (self.pos.0 - another.pos.0).abs() < error_limit &&
+            (self.pos.1 - another.pos.1).abs() < error_limit &&
+            (self.pos.2 - another.pos.2).abs() < error_limit;
     }
-    // compare atoms position under periodic boundary checking
-    pub(crate) fn is_near_eq_with_pbc(&self, another: &Particle, error_limit: f64, (box_x, box_y, box_z): (f64, f64, f64)) -> bool {
-        return ((self.x - another.x).abs() < error_limit || ((self.x - another.x).abs() - box_x).abs() < error_limit)
-            && ((self.y - another.y).abs() < error_limit || ((self.y - another.y).abs() - box_y).abs() < error_limit)
-            && ((self.z - another.z).abs() < error_limit || ((self.z - another.z).abs() - box_z).abs() < error_limit)
-            && (self.vx - another.vx).abs() < error_limit
-            && (self.vy - another.vy).abs() < error_limit
-            && (self.vz - another.vz).abs() < error_limit;
+
+    // check 2 atoms' position with an error limit and periodic boundary condition.
+    pub(crate) fn is_pos_near_eq_with_pbc(&self, another: &Particle, error_limit: f64, (box_x, box_y, box_z): (f64, f64, f64)) -> bool {
+        return ((self.pos.0 - another.pos.0).abs() < error_limit || ((self.pos.0 - another.pos.0).abs() - box_x).abs() < error_limit)
+            && ((self.pos.1 - another.pos.1).abs() < error_limit || ((self.pos.1 - another.pos.1).abs() - box_y).abs() < error_limit)
+            && ((self.pos.2 - another.pos.2).abs() < error_limit || ((self.pos.2 - another.pos.2).abs() - box_z).abs() < error_limit);
+    }
+
+    fn is_velocity_near_eq(&self, another: &Particle, error_limit: f64) -> bool {
+        return (self.v.0 - another.v.0).abs() < error_limit &&
+            (self.v.1 - another.v.1).abs() < error_limit &&
+            (self.v.2 - another.v.2).abs() < error_limit;
+    }
+
+
+    // check 2 atoms' status, including position, and possible velocity, force.
+    pub(crate) fn is_status_near_eq(&self, another: &Particle, error_limit: f64) -> bool {
+        return self.is_pos_near_eq(another, error_limit)
+            && self.is_velocity_near_eq(another, error_limit);
+    }
+    // compare atoms status under periodic boundary checking
+    pub(crate) fn is_status_near_eq_with_pbc(&self, another: &Particle, error_limit: f64, box_length: (f64, f64, f64)) -> bool {
+        return self.is_pos_near_eq_with_pbc(another, error_limit, box_length)
+            && self.is_velocity_near_eq(another, error_limit);
     }
 }
 
 impl FromStr for Particle {
     type Err = Error;
 
+    // the line must include id and type. while position, v and force are optional.
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let splitted: Vec<&str> = s.split_whitespace().collect();
-        if splitted.len() != 7 {
-            return Err(Error::IllegalState(String::from("")));
+        if splitted.len() < 1 {
+            return Err(Error::IllegalState(String::from("must specific id and type")));
         }
+        let mut pos: (Real, Real, Real) = (0.0, 0.0, 0.0);
+        let mut v: (Real, Real, Real) = (0.0, 0.0, 0.0);
+
+        if splitted.len() < 1 + 3 {
+            pos = (splitted[2].parse::<Real>().unwrap(), splitted[3].parse::<Real>().unwrap(), splitted[4].parse::<Real>().unwrap())
+        }
+        if splitted.len() < 1 + 3 + 3 {
+            v = (splitted[5].parse::<Real>().unwrap(), splitted[6].parse::<Real>().unwrap(), splitted[7].parse::<Real>().unwrap())
+        }
+
         Ok(Particle {
             id: splitted[0].parse::<Real>()? as u32,
-            x: splitted[1].parse::<Real>()?,
-            y: splitted[2].parse::<Real>()?,
-            z: splitted[3].parse::<Real>()?,
-            vx: splitted[4].parse::<Real>()?,
-            vy: splitted[6].parse::<Real>()?,
-            vz: splitted[6].parse::<Real>()?,
+            pos,
+            v,
         })
     }
 }
@@ -62,7 +80,7 @@ impl FromStr for Particle {
 impl ToString for Particle {
     fn to_string(&self) -> String {
         format!("id: {}, position: ({}, {}, {}), v:({}, {}, {})",
-                self.id, self.x, self.y, self.z, self.vx, self.vy, self.vz)
+                self.id, self.pos.0, self.pos.1, self.pos.1, self.v.0, self.v.1, self.v.2)
     }
 }
 
