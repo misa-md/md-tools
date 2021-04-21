f889dbfb3e0db8f34aa882365b5e1a26a25d5c69
genshen <genshenchu@gmail.com>
2021-04-21 17:21:13 +0800

    feat(diff): add force and type fields to Particle, add ability to parse and compare these fields
    We add force and type fields to struct `Particle`. And add ability to parse and compare force and
    type fields of partiles.

--- src/xyz/particle.rs
+++ src/xyz/particle.rs
@@ -12,8 +12,11 @@ type Real = f64;
 // full information of a particle
 pub struct Particle {
     pub id: u32,
+    // type of atom
+    pub tp: String,
     pub pos: (Real, Real, Real),
     pub v: (Real, Real, Real),
+    pub f: (Real, Real, Real),
 }
 
 impl Particle {
@@ -37,16 +40,23 @@ impl Particle {
             (self.v.2 - another.v.2).abs() < error_limit;
     }
 
+    fn is_force_near_eq(&self, another: &Particle, error_limit: f64) -> bool {
+        return (self.f.0 - another.f.0).abs() < error_limit &&
+            (self.f.1 - another.f.1).abs() < error_limit &&
+            (self.f.2 - another.f.2).abs() < error_limit;
+    }
 
     // check 2 atoms' status, including position, and possible velocity, force.
     pub(crate) fn is_status_near_eq(&self, another: &Particle, error_limit: f64) -> bool {
         return self.is_pos_near_eq(another, error_limit)
-            && self.is_velocity_near_eq(another, error_limit);
+            && self.is_velocity_near_eq(another, error_limit)
+            && self.is_force_near_eq(another, error_limit);
     }
     // compare atoms status under periodic boundary checking
     pub(crate) fn is_status_near_eq_with_pbc(&self, another: &Particle, error_limit: f64, box_length: (f64, f64, f64)) -> bool {
         return self.is_pos_near_eq_with_pbc(another, error_limit, box_length)
-            && self.is_velocity_near_eq(another, error_limit);
+            && self.is_velocity_near_eq(another, error_limit)
+            && self.is_force_near_eq(another, error_limit);
     }
 }
 
@@ -56,23 +66,28 @@ impl FromStr for Particle {
     // the line must include id and type. while position, v and force are optional.
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let splitted: Vec<&str> = s.split_whitespace().collect();
-        if splitted.len() < 1 {
+        if splitted.len() < 2 {
             return Err(Error::IllegalState(String::from("must specific id and type")));
         }
         let mut pos: (Real, Real, Real) = (0.0, 0.0, 0.0);
         let mut v: (Real, Real, Real) = (0.0, 0.0, 0.0);
+        let mut f: (Real, Real, Real) = (0.0, 0.0, 0.0);
 
-        if splitted.len() < 1 + 3 {
+        if splitted.len() < 2 + 3 {
             pos = (splitted[2].parse::<Real>().unwrap(), splitted[3].parse::<Real>().unwrap(), splitted[4].parse::<Real>().unwrap())
         }
-        if splitted.len() < 1 + 3 + 3 {
+        if splitted.len() < 2 + 3 + 3 {
             v = (splitted[5].parse::<Real>().unwrap(), splitted[6].parse::<Real>().unwrap(), splitted[7].parse::<Real>().unwrap())
         }
-
+        if splitted.len() < 2 + 3 + 3 + 3 {
+            f = (splitted[8].parse::<Real>().unwrap(), splitted[9].parse::<Real>().unwrap(), splitted[10].parse::<Real>().unwrap())
+        }
         Ok(Particle {
             id: splitted[0].parse::<Real>()? as u32,
+            tp: splitted[1].parse().unwrap(),
             pos,
             v,
+            f,
         })
     }
 }
