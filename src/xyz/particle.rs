989bcdf99ae765a7084bd74705106d4633509c22
genshen <genshenchu@gmail.com>
2021-06-21 11:20:25 +0800

    refactor(diff): move velocity and force filed of `Particle` to vector field `extra_data`

--- src/xyz/particle.rs
+++ src/xyz/particle.rs
@@ -9,14 +9,13 @@ use xyzio::Error;
 
 type Real = f64;
 
-// full information of a particle
+// full information of a particle (e.g. position and velocity).
 pub struct Particle {
     pub id: u32,
     // type of atom
     pub tp: String,
     pub pos: (Real, Real, Real),
-    pub v: (Real, Real, Real),
-    pub f: (Real, Real, Real),
+    pub extra_data: Vec<Real>,
 }
 
 impl Particle {
@@ -34,68 +33,69 @@ impl Particle {
             && ((self.pos.2 - another.pos.2).abs() < error_limit || ((self.pos.2 - another.pos.2).abs() - box_z).abs() < error_limit);
     }
 
-    fn is_velocity_near_eq(&self, another: &Particle, error_limit: f64) -> bool {
-        return (self.v.0 - another.v.0).abs() < error_limit &&
-            (self.v.1 - another.v.1).abs() < error_limit &&
-            (self.v.2 - another.v.2).abs() < error_limit;
-    }
-
-    fn is_force_near_eq(&self, another: &Particle, error_limit: f64) -> bool {
-        return (self.f.0 - another.f.0).abs() < error_limit &&
-            (self.f.1 - another.f.1).abs() < error_limit &&
-            (self.f.2 - another.f.2).abs() < error_limit;
+    fn compare_extra_data(&self, another: &Particle, error_limit: f64) -> bool {
+        if self.extra_data.len() != another.extra_data.len() {
+            return false;
+        }
+        let mut i = 0;
+        for real_value in &self.extra_data {
+            if (real_value - another.extra_data[i]).abs() >= error_limit {
+                return false;
+            }
+            i += 1;
+        }
+        return true;
     }
 
     // check 2 atoms' status, including position, and possible velocity, force.
     pub(crate) fn is_status_near_eq(&self, another: &Particle, error_limit: f64) -> bool {
-        return self.is_pos_near_eq(another, error_limit)
-            && self.is_velocity_near_eq(another, error_limit)
-            && self.is_force_near_eq(another, error_limit);
+        return self.is_pos_near_eq(another, error_limit) && self.compare_extra_data(another, error_limit);
     }
+
     // compare atoms status under periodic boundary checking
     pub(crate) fn is_status_near_eq_with_pbc(&self, another: &Particle, error_limit: f64, box_length: (f64, f64, f64)) -> bool {
         return self.is_pos_near_eq_with_pbc(another, error_limit, box_length)
-            && self.is_velocity_near_eq(another, error_limit)
-            && self.is_force_near_eq(another, error_limit);
+            && self.compare_extra_data(another, error_limit);
     }
 }
 
 impl FromStr for Particle {
     type Err = Error;
 
-    // the line must include id and type. while position, v and force are optional.
+    // the line must include id and type and position. while velocity, force and other data are optional.
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let splitted: Vec<&str> = s.split_whitespace().collect();
-        if splitted.len() < 2 {
-            return Err(Error::IllegalState(String::from("must specific id and type")));
+        let split: Vec<&str> = s.split_whitespace().collect();
+        if split.len() < 5 || split.len() % 3 != 2 {
+            return Err(Error::IllegalState(String::from("must specific id, type and position")));
         }
-        let mut pos: (Real, Real, Real) = (0.0, 0.0, 0.0);
-        let mut v: (Real, Real, Real) = (0.0, 0.0, 0.0);
-        let mut f: (Real, Real, Real) = (0.0, 0.0, 0.0);
 
-        if splitted.len() < 2 + 3 {
-            pos = (splitted[2].parse::<Real>().unwrap(), splitted[3].parse::<Real>().unwrap(), splitted[4].parse::<Real>().unwrap())
-        }
-        if splitted.len() < 2 + 3 + 3 {
-            v = (splitted[5].parse::<Real>().unwrap(), splitted[6].parse::<Real>().unwrap(), splitted[7].parse::<Real>().unwrap())
-        }
-        if splitted.len() < 2 + 3 + 3 + 3 {
-            f = (splitted[8].parse::<Real>().unwrap(), splitted[9].parse::<Real>().unwrap(), splitted[10].parse::<Real>().unwrap())
-        }
-        Ok(Particle {
-            id: splitted[0].parse::<Real>()? as u32,
-            tp: splitted[1].parse().unwrap(),
+        let mut pos: (Real, Real, Real) = (
+            split[2].parse::<Real>().unwrap(),
+            split[3].parse::<Real>().unwrap(),
+            split[4].parse::<Real>().unwrap(),
+        );
+
+        let mut particle = Particle {
+            id: split[0].parse::<Real>()? as u32,
+            tp: split[1].parse().unwrap(),
             pos,
-            v,
-            f,
-        })
+            extra_data: vec![],
+        };
+        let mut i = 0;
+        for real_str in split {
+            if i >= 5 {
+                particle.extra_data.push(real_str.parse::<Real>()?);
+            }
+            i += 1;
+        }
+        Ok(particle)
     }
 }
 
 impl ToString for Particle {
     fn to_string(&self) -> String {
-        format!("id: {}, position: ({}, {}, {}), v:({}, {}, {})",
-                self.id, self.pos.0, self.pos.1, self.pos.1, self.v.0, self.v.1, self.v.2)
+        format!("id: {}, position: ({}, {}, {}), extra_data: {:?})",
+                self.id, self.pos.0, self.pos.1, self.pos.2, self.extra_data)
     }
 }
 
